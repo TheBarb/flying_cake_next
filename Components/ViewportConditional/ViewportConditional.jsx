d09a522f6379cd44063f804e027af510126546b7
Barbara Schulze <mail@barbaraschulze.de>
2022-11-08 16:23:10 +0100

    fetch and filter data

---
+++ Components/ViewportConditional/ViewportConditional.jsx
@@ -0,0 +1,10 @@
+import styles from './viewportConditional.module.scss'
+
+
+const ViewportConditional = () => {
+  return (
+    <div className={styles.test}>ViewportConditional</div>
+  )
+}
+
+export default ViewportConditional
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,41 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import ViewportConditional from '../Components/ViewportConditional/ViewportConditional'
 
-export default function Home() {
+
+
+const Home = ({cakes, sweetCakes, saltyCakes}) => {
   return (
     <>
-      <Head>
-        <title>FLYING CAKE</title>
-        <meta name="description" content="EAT MORE CAKE" />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
+    <Head>
+      <title>FLYING CAKE</title>
+      <meta name="description" content="EAT MORE CAKE" />
+      <link rel="icon" href="/favicon.svg" />
+    </Head>
 
-      <main className={styles.main}>
-       <div>EAT MORE CAKE</div>
-      </main>
+    <main className={styles.main}>
+    <ViewportConditional cakes={cakes} sweetCakes={sweetCakes} saltyCakes={saltyCakes}/>
+    </main>
 
-    
-    </>
+  
+  </>
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:1337/api/products?pagination[page]=1&pagination[pageSize]=50')
+  const products = await res.json()
+  const cakes = products.data
+  const sweetCakes = cakes.filter(cake => cake.attributes.sweetnotsalty)
+  const saltyCakes = cakes.filter(cake => cake.attributes.sweetnotsalty === false)
+  
+  return {
+    props: {
+      cakes: cakes,
+      sweetCakes: sweetCakes,
+      saltyCakes: saltyCakes
+    }
+  }
+}
+
+export default Home
